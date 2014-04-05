11a11babdbdcee8922d368c7b3d4b6188c50b463
Scott Barr <scott@signalvsnoise.com>
2014-04-06 01:16:51 +1030

    Added the remainder of the API in.

--- itbit.py
+++ itbit.py
@@ -10,17 +10,25 @@ class Itbit(object):
         self.symbol = symbol
 
 
-    def url(self, resource):
-        return "%s/%s" % (Itbit.HOST, resource)
-
-
-    def json(self, resource):
-        return requests.get(self.url(resource)).json()
-
-
     def ticker(self):
-        return self.json('ticker/%s' % self.symbol)
+        return self._json('ticker/%s' % self.symbol)
+
+
+    def price_feed(self):
+        return self._json('market/%s' % self.symbol)
 
 
     def order_book(self):
-        return self.json('/orderbook/%s' % self.symbol)
+        return self._json('orderbook/%s' % self.symbol)
+
+
+    def news(self, max=10):
+        return self._json('news/%i' % (max))
+
+
+    def _url(self, resource):
+        return "%s/%s" % (Itbit.HOST, resource)
+
+
+    def _json(self, resource):
+        return requests.get(self._url(resource)).json()
